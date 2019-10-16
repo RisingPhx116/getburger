f349993c795996a8d00e5363132d951e36f26631
Pat Perry <risingphx1@gmail.com>
2019-10-16 18:47:45 -0400

    updated sql, will check the code

--- shema.sql
+++ shema.sql
@@ -8,11 +8,11 @@ USE getburger_DB;
 -- Create the table plans.
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
-  burgers varchar(255) NOT NULL,
+  burger varchar(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
 -- Insert a set of records.
-INSERT INTO burgers (burgers) VALUES ('The Dallas Cowboy Burger');
-INSERT INTO burgers (burgers) VALUES ('Da Mac & Cheese Burger');
-INSERT INTO burgers (burgers) VALUES ('Peppery Pepper Jack Burger');
+INSERT INTO burgers (burger) VALUES ('The Dallas Cowboy Burger');
+INSERT INTO burgers (burger) VALUES ('Da Mac & Cheese Burger');
+INSERT INTO burgers (burger) VALUES ('Peppery Pepper Jack Burger');
